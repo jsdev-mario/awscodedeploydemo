4396ec0645006b2dbdd59886f0112b9c6048c5e7
Bk3131 <bk313131@outlook.com>
2020-06-04 07:25:32 +0800

    test

--- dist/static/custom-sw.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener("push", (event) => {
-  const data = event.data.json();
-  const options = {
-    body: data.body,
-  };
-  event.waitUntil(self.registration.showNotification(data.title, options));
-});
